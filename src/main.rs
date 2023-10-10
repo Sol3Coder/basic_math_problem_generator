f85d3e6c15ec4fa4ab53177b039ed76e6abe15a8
whl <ewanghailong@vip.qq.com>
2023-10-10 10:48:37 +0800

    修改控制流，初步完善三、四个操作数逻辑

--- src/main.rs
+++ src/main.rs
@@ -39,45 +39,41 @@ pub fn main() {
 
 fn gen_doc(user_input:UserInput){
 
-    println!("{}",user_input.type_index);
-    println!("{}",user_input.range_index);
-    println!("{}",user_input.cnt_index);
+
  
-    let mut operator_symbol = '+';
-    match user_input.type_index {
+    let  operator_symbol =  match user_input.type_index {
         0=>{
-            operator_symbol= '+';
+            '+'
         },
         1=>{
-            operator_symbol= '-';
+           '-'
         },
         2=>{
-            operator_symbol='×';
+            '×'
         },
         3=>{
-            operator_symbol= '÷';
+             '÷'
         },   
         _=>{
-            operator_symbol='+';
+            '+'
         }
     };
-    let mut max_num = 10;
-    match user_input.range_index {
+    let max_num = match user_input.range_index {
         0=>{
-            max_num= 10;
+           10
         },
         1=>{
-            max_num= 20;
+          20
         },
         2=>{
-            max_num= 50;
+           50
         },
         3=>{
-            max_num= 100;
+            100
         }
         _=>{
-            max_num= 10;}
-    }
+            10}
+    };
 
     let (doc, page1, layer1) = PdfDocument::new("PDF_Document_title", Mm(210.0), Mm(297.0), "Layer 1");
     let current_layer = doc.get_page(page1).get_layer(layer1);
@@ -90,22 +86,20 @@ fn gen_doc(user_input:UserInput){
     for row in 0..=24 {
 
         for col in 0..=2{
-            let mut x_pos = x_item1;
-            match col{
+            let x_pos = match col{
                 0=>{
-                    x_pos = x_item1;
+                    x_item1
                 },
                 1=>{
-                    x_pos = x_item2;
+                    x_item2
                 },
                 2=>{
-                    x_pos = x_item3;
+                    x_item3
                 },  
                 _=>{
-                    x_pos = x_item1;
-
+                    x_item1
                 }
-            }
+            };
             let mut operat_num1 = rng.gen_range(1..max_num);
             let mut operat_num2 = rng.gen_range(1..max_num);
             if  operator_symbol == '-' || operator_symbol == '÷' {
@@ -113,10 +107,53 @@ fn gen_doc(user_input:UserInput){
                 operat_num2 = rng.gen_range(1..operat_num1);
 
             }
-            let cur_text = format!("{item1} {operator_symbol} {item2} =   ",
-            item1 = operat_num1,
-            operator_symbol = operator_symbol,
-            item2 = operat_num2);
+    
+
+            let cur_text = match user_input.cnt_index {
+                0=>{
+                    format!("{item1} {operator_symbol} {item2} =   ",
+                    item1 = operat_num1,
+                    operator_symbol = operator_symbol,
+                    item2 = operat_num2)
+                },
+                1=>{
+                    let mut operat_num3 = rng.gen_range(1..max_num);
+                    if  operator_symbol == '-' || operator_symbol == '÷' {
+                        operat_num1 = rng.gen_range(3..max_num);
+                        operat_num2 = rng.gen_range(2..operat_num1);
+                        operat_num3 = rng.gen_range(1..operat_num2);
+
+                    }
+                    format!("{item1} {operator_symbol} {item2} {operator_symbol} {item3} =   ",
+                    item1 = operat_num1,
+                    operator_symbol = operator_symbol,
+                    item2 = operat_num2,
+                    item3 = operat_num3)
+                },
+                2=>{
+                    let mut operat_num3 = rng.gen_range(1..max_num);
+                    let mut operat_num4 = rng.gen_range(1..max_num);
+                    if  operator_symbol == '-' || operator_symbol == '÷' {
+                        operat_num1 = rng.gen_range(4..max_num);
+                        operat_num2 = rng.gen_range(3..operat_num1);
+                        operat_num3 = rng.gen_range(2..operat_num2);
+                        operat_num4 = rng.gen_range(1..operat_num3);
+
+                    }
+                    format!("{item1} {operator_symbol} {item2} {operator_symbol} {item3} {operator_symbol} {item4}=   ",
+                    item1 = operat_num1,
+                    operator_symbol = operator_symbol,
+                    item2 = operat_num2,
+                    item3 = operat_num3,
+                    item4 = operat_num4)
+                },
+                _=>{
+                    format!("{item1} {operator_symbol} {item2} =   ",
+                    item1 = operat_num1,
+                    operator_symbol = operator_symbol,
+                    item2 = operat_num2)
+                }
+            };
             current_layer.use_text(cur_text,  15.0, Mm(x_pos), Mm(270.0-row as f32 *(270.0/25.0)), &font);
         }
 
